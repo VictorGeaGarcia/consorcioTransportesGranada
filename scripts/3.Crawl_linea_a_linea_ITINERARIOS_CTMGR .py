5078d8eb412081dd806b881f11406d20c8a784ce
Victor Gea <Victor Gea>
2017-06-29 15:50:21 +0200

    Script 3 Finished. PEP8.ENGLISH.FUNCS.DOCSTRINGS.DB. DB POPULATED WITH ITINERARIES

--- scripts/3.Crawl_linea_a_linea_ITINERARIOS_CTMGR .py
+++ scripts/3.Crawl_linea_a_linea_ITINERARIOS_CTMGR .py
@@ -1,75 +1,64 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 import re
+import requests
 
-#Leemos CSV que nos interesa para ir haciendo Crawl linea a linea
-consorcio_lineasDF = pd.read_csv ('Consorcio_Lineas_Granada.csv',index_col = 'Unnamed: 0')
+import sqlite3
+import pandas as pd
 
+from utilities.getting_soup_from_web import BSoup
 
-#Hacemos un crawl primero a cualquiera de las lineas para ver exacatamente que hay que
-#extraer en cada caso
+def fill_db(bus_line, one_way_or_return, bus_line_stopsDF):
+    '''FILL CONSORCIOGR.DB WITH ITINERARY FOR EACH BUS LINE'''
+    db_name = '..\datos\ConsorcioGR.db'
+    table_name = 'itinerary_{0}_{1}_CTMGR'.format(bus_line, one_way_or_return)
+    conn_CTMGR = sqlite3.connect(db_name)
+    bus_line_stopsDF.to_sql(table_name, conn_CTMGR, index= False)
+    conn_CTMGR.commit()
+    conn_CTMGR.close()
 
-lineas = consorcio_lineasDF.NumLineaWeb.values
-##for linea in lineas:
-##    print(linea)
+def obtain_itinerary(bus_line,itinerary_soup,id_way_or_return):
+    '''CRAWL WEBPAGES AND FILLS DF WITH ITINERARIES'''
+    bus_line_stopsDF = pd.DataFrame(columns=['bus_stop','lat','lon'])
+    try:
+        itinerary = itinerary_soup.find(id = id_way_or_return).find_all('a')
+    except AttributeError:
+        print('{0} LINEA RAISED SOME ERROR'.format(bus_line))
+    else:
+        for link in itinerary:
+            coord_begin = int(re.search('\(',link['href']).start())
+            coord_end = int(re.search('\)',link['href']).end())
+            lat , lon = link['href'][coord_begin+1:coord_end-1].split(',')
+            bus_stop_name = link.text
 
-
-###
-###   TODO LO DE ABAJO HAY QUE METERLO EN EL FOR QUE HAY JUSTO ARRIBA (for linea in lineas)
-###
-
-   #########################################################################################
-
-for linea in lineas:
-##    print('Estamos en la linea: ',linea)
-    url = 'http://siu.ctagr.com/es/horarios_lineas_tabla.php?from=1&linea={}'.format(linea)
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text)
-##    print(soup)
-
-
-    ida_lineaDF = pd.DataFrame(columns=['Parada','Lat','Lon'])
-
-    itinerario_ida = soup.find(id = 'paradas_ida').find_all('a')
-    paradas_ida = []
-    ind = 0
-##    print('IDA')
-    for link in itinerario_ida:
-
-        inicio_coord = int(re.search('\(',link['href']).start())
-        fin_coord = int(re.search('\)',link['href']).end())
-        lat , lon = link['href'][inicio_coord+1:fin_coord-1].split(',')
-        nom_parada = link.text
-        paradas_ida.append(nom_parada)
+            bus_line_stopsDF = bus_line_stopsDF.append(pd.DataFrame(
+                data=[[bus_stop_name,lat,lon]],columns=['bus_stop','lat','lon']))
+##        bus_line_stopsDF.to_csv(
+##            '{0}_{1}_itinerary_CTMGR.csv'.format(bus_line,id_way_or_return))
+        fill_db(bus_line,  id_way_or_return,  bus_line_stopsDF)
         
-        ida_lineaDF = ida_lineaDF.append(pd.DataFrame(data=[[nom_parada,lat,lon]],columns=['Parada','Lat','Lon']))
-        ind =+ 1    
-##        print(link['href'])
-##        print('Lat: ',lat,' Lon: ',lon)
-##        print('La parada es :',nom_parada)
+def read_csv():
+    '''READS CSV WITH GENERAL DATA NEEDED FOR CRAWLING AND RETURNS
+       WEB_CODES FOR EACH LINE'''
+    encoding_library = ['iso-8859-1',  'latin1',  'cp1252',  'utf-8']
+    for encoding in encoding_library:
+        try:
+            consorcio_lineasDF = pd.read_csv (
+                '..\datos\Consorcio_bus_lines_Granada.csv',
+                index_col = 'Unnamed: 0',   encoding=encoding)
+        except UnicodeDecodeError:
+            print('Encoding with {0} didn\'t work'.format(encoding))
+        else:
+            print('Encoding with {0} succesful'.format(encoding))
+            break
+    return consorcio_lineasDF.bus_line_web_code.values
 
-    ida_lineaDF.to_csv('{}_ida_itinerario_CTMGR.csv'.format(linea))
-        
-    vuelta_lineaDF = pd.DataFrame(columns=['Parada','Lat','Lon'])
-
-    itinerario_vuelta = soup.find(id = 'paradas_vuelta').find_all('a')
-    paradas_vuelta = []
-##    print('VUELTA')
-    for link in itinerario_vuelta:
-
-        inicio_coord = int(re.search('\(',link['href']).start())
-        fin_coord = int(re.search('\)',link['href']).end())
-        lat , lon = link['href'][inicio_coord+1:fin_coord-1].split(',')
-        nom_parada = link.text
-        paradas_vuelta.append(nom_parada)
-        
-        vuelta_lineaDF = ida_lineaDF.append(pd.DataFrame(data=[[nom_parada,lat,lon]],columns=['Parada','Lat','Lon']))
-        ind =+ 1    
-##        print(link['href'])
-##        print('Lat: ',lat,' Lon: ',lon)
-##        print('La parada es :',nom_parada)
-
-##    print(vuelta_lineaDF)
-    vuelta_lineaDF.to_csv('{}_vuelta_itinerario_CTMGR.csv'.format(linea))
+def main():
+    '''GETS LIST OF WEB_CODES NEEDED TO CRAWL. FOR EACH LINE GETS ITS
+    SOUP AND CALLS FUNCTION TO OBTAIN TIMETABLES'''
+    lineas = read_csv()
+    for linea in lineas:
+        url = 'http://siu.ctagr.com/es/horarios_lineas_tabla.php?from=1&linea={0}'
+        url = url.format(linea)
+        itinerary_soup = BSoup(url)
+        for id_way_or_return in ['paradas_ida',  'paradas_vuelta']:
+            obtain_itinerary(linea,itinerary_soup, id_way_or_return)
+main()
